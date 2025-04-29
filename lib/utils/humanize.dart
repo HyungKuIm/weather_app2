014dcb8b67d69d1073459152763621e9f019f3e9
HyungKu Im <damansa1@naver.com>
2025-04-29 17:56:09 +0900

    Update humanize.dart

--- lib/utils/humanize.dart
+++ lib/utils/humanize.dart
@@ -1,7 +1,7 @@
 import 'package:weather_app2/models/weather.dart';
 import 'package:weather_app2/utils/weather_utils.dart';
 
-import 'DateUtils.dart';
+import 'DateUtils.dart' as dt;
 
 class Humanize {
 
@@ -11,5 +11,14 @@ class Humanize {
       tempInt = Temperature.celsiusToFahrenheit(temp.temperature.current);
     }
     return '$tempInt ${WeatherUtil.temperatureLabels[unit]}';
+  }
+
+  //날씨 정보 취득
+  static String weatherDescription(Weather weather) {
+    // 요일 취득
+    var day = dt.DateUtils.weekdays[weather.dateTime.weekday];
+    var description = Weather.displayValues[weather.weatherDescription];
+    return "$day, $description";
+
   }
 }
